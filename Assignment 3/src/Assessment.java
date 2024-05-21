a395580af6c8cd24f027edd436d14a48ddc28774
Connor Morrison <113166048+connormorrrison@users.noreply.github.com>
2024-05-20 23:43:28 -0600

    Add initial skeleton for Assessment class based on UML diagram

--- Assignment 3/src/Assessment.java
+++ Assignment 3/src/Assessment.java
@@ -1,2 +1,13 @@
 public class Assessment {
+    protected int total;
+    protected int score;
+
+    public Assessment(int total, int score) {
+    }
+
+    public int getScore() {
+    }
+
+    public int getTotal() {
+    }
 }
