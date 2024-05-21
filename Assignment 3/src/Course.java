490382c03f6c2adf92d3f0ab85d69d29c8f66978
Connor Morrison <113166048+connormorrrison@users.noreply.github.com>
2024-05-20 23:33:09 -0600

    Add initial skeleton for Course class based on UML diagram

--- Assignment 3/src/Course.java
+++ Assignment 3/src/Course.java
@@ -1,2 +1,37 @@
 public class Course {
+    private String courseName;
+    private Assignment[] assignments;
+    private int assignmentIdx;
+    private Quiz[] quizzes;
+    private int quizIdx;
+    private Midterm midterm;
+    private LabExam labExam;
+    private FinalExam finalExam;
+
+    public Course(String name, int numAssignments, int numQuizzes) {
+    }
+
+    public void addAssignment(int total, int score) {
+    }
+
+    public void addQuiz(int total, int score) {
+    }
+
+    public void addMidterm(int total, int score) {
+    }
+
+    public void addLabExam(int total, int score) {
+    }
+
+    public void addFinalExam(int total, int score) {
+    }
+
+    public double getFinalGrade() {
+    }
+
+    public String getCourseName() {
+    }
+
+    public String toString() {
+    }
 }
