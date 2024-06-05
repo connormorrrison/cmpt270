9f7fb70df6202d52a5f3fe76bac02a0b9d676f37
Connor Morrison <113166048+connormorrrison@users.noreply.github.com>
2024-06-05 15:01:25 -0600

    Added function skeletons

--- Assignment 4/src/Stack.java
+++ Assignment 4/src/Stack.java
@@ -5,5 +5,32 @@
  * Course: CMPT 270 Section 01
  */
 
-public class Stack {
+import java.util.LinkedList;
+
+public class Stack<I> implements StackOperations<I> {
+    private LinkedList<I> stackLinkedList;
+
+    public Stack() {
+        stackLinkedList = new LinkedList<>();
+    }
+
+    // void push(I item);
+    public void push(I item) {
+    }
+
+    // void pop();
+    public void pop() {
+    }
+
+    // I peek();
+    public I peek() {
+    }
+
+    // int size();
+    public int size() {
+    }
+
+    // boolean isEmpty();
+    public boolean isEmpty() {
+    }
 }
